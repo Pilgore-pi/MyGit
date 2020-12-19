ecb1b79a190631bfffdf437ea7e3c2b682146840
Андрей <andrey.prishutov@gmail.com>
2020-12-19 17:23:09 +0300

    Second commit

---
+++ Work/proga.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int A, B, C;
+    cout << "Введите числа" << endl;
+    cout << "A = "; cin >> A;
+    cout << "B = "; cin >> B;
+    cout << "C = "; cin >> C;
+    if (A > B) {
+        if (A > C) cout << "Наибольшее число " << A;
+        else cout << "Наибольшее число " << C;
+    }
+    else {
+        if(B > C) cout << "Наибольшее число " << B;
+        else cout << "Наибольшее число " << C;
+    }
+    return 0;
+}
